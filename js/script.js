6753491342bc24ee4eb7dee22117c98ef90cebe7
yewon_s <seoyewon93@gmail.com>
2023-05-10 16:22:36 -0400

    Created guest list

--- js/script.js
+++ js/script.js
@@ -10,3 +10,35 @@ const guestList = document.querySelector(".guest-list");
 const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
+
+addGuestButton.addEventListener("click", function () {
+    const guest = guestInput.value;
+    // console.log(guest);
+    if (guest !== "") {
+        addToList(guest);
+        clearInput();
+        updateGuestCount();
+    }
+});
+
+const clearInput = function () {
+    guestInput.value = "";
+};
+
+const addToList = function (guest) {
+    const listItem = document.createElement("li");
+    listItem.innerText = guest;
+    guestList.append(listItem);
+};
+
+const updateGuestCount = function () {
+    const guests = document.querySelectorAll(".guest-list li");
+    guestCount.innerText = guests.length;
+
+    if (guests.length === 8) {
+        addGuestButton.classList.add("hide");
+        guestInput.classList.add("hide");
+        guestInputLabel.classList.add("hide");
+        guestFull.classList.remove("hide");
+    }
+};
